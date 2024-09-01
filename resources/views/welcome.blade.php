c5f670d113ce2f89c6fb9994ec31577a0dd1e96a
VasyaTuryak <vasyaturyak87@gmail.com>
2024-09-01 22:18:32 +0300

    add admin panel to project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,14 @@
 <body>
 <div style="height: 70%; display: flex; justify-content: center; align-items: center">
     <div style="text-align: center;">
+
         <h1>Hello World</h1>
         <p>Це BackEnd, для сайту з українськими вишиванками.</p>
         <p><b>Проект Team Challenge.</b> </p>
+        <div class="mt-20 flex flex-row justify-between">
+            <a class="text-red-700 text-2xl mr-5" href="{{ route('apiDocs') }}">Go to API Docs</a>
+            <a class="text-red-700 text-2xl" href="{{ route('login') }}">Go to Admin Panel</a>
+        </div>
     </div>
 </div>
 </body>
