1208ebd2e507af884d200caf77a44705bd9c8c3c
VasyaTuryak <vasyaturyak87@gmail.com>
2024-10-21 22:49:15 +0300

    add filter 21-10-2024

---
+++ app/Http/Requests/UserFormRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'phone' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.unique' => 'The email you gave, has already been taken.',
+            'phone.unique' => 'The phone you gave, has already been taken.',
+        ];
+    }
+}
